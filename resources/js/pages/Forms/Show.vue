33fbca60e8efd82635a9d644b0f5305c0502eeb9
dodger215 <kelvinenosdzah2@gmail.com>
2026-06-20 19:08:48 +0000

    Fixed location

--- resources/js/pages/Forms/Show.vue
+++ resources/js/pages/Forms/Show.vue
@@ -281,6 +281,7 @@ const getInputType = (fieldType: string) => {
                     :class="`w-full px-4 py-2 bg-slate-900 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-blue-500 ${getFieldIcon(field.type) ? 'pl-10' : ''}`"
                   />
                 </div>
+              </div>
             </div>
           </div>
         </div>
